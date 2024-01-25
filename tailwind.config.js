82b102a409debba7f96e0fdee23021c9ea02e5bc
Javad Yousefi <javadyousefi9000@gmail.com>
2024-01-25 12:15:11 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#163020",
+        secondary: "#304D30",
+        light_primary: "#B6C4B6",
+        main_white: "#EEF0E5"
+      },
+    },
+  },
+  plugins: [],
+};
